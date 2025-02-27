21b6989b038ef019434106c1795ec28b2a7006b6
Mohammed Hasan <hasanazeerkhan@gmail.com>
2025-02-27 21:42:27 +0530

    feat: added new tests

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
 import { testResponseCode } from "./function.js";
+import { verifyAuth } from "./login.js";
+import {loginSuccessful , registerSuccessful} from './data.js';
+
 
 testResponseCode.get("/api/users/2", 200);
 testResponseCode.delete("/api/users/2", 204);
+
+const auth = new verifyAuth();
+auth.registerSuccess("/api/register", registerSuccessful)
+auth.loginSuccess("/api/login", loginSuccessful);
