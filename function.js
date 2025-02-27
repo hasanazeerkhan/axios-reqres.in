08c7db6984357e201253876c0daba44f340e781e
Mohammed Hasan <hasanazeerkhan@gmail.com>
2025-02-27 18:46:48 +0530

    feat: added new functions

--- function.js
+++ function.js
@@ -1,8 +1,8 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 import axios from 'axios';
+const baseUrl = "https://reqres.in"
 
 export const testResponseCode = {
-    baseUrl: "https://reqres.in",
     validateResponse(expected, observed) {
         try {
             if (expected != observed) {
@@ -15,30 +15,30 @@ export const testResponseCode = {
         }
     },
     async get(requestURL, expectedResponseCode) {
-        const response = await axios.get(testResponseCode.baseUrl + requestURL);
+        const response = await axios.get(baseUrl + requestURL);
         testResponseCode.validateResponse(expectedResponseCode, response.status);       
     },
 
     async post(requestURL, expectedResponseCode) {
-        const response = await axios.post(testResponseCode.baseUrl + requestURL, requestBody);
+        const response = await axios.post(baseUrl + requestURL, requestBody);
         testResponseCode.validateResponse(expectedResponseCode, response.status);
 
     },
 
     async put(requestURL, expectedResponseCode) {
-        const response = await axios.put(testResponseCode.baseUrl + requestURL, requestBody);
+        const response = await axios.put(baseUrl + requestURL, requestBody);
         testResponseCode.validateResponse(expectedResponseCode, response.status);
 
     },
 
     async patch(requestURL, expectedResponseCode) {
-        const response = await axios.patch(testResponseCode.baseUrl + requestURL, requestBody);
+        const response = await axios.patch(baseUrl + requestURL, requestBody);
         testResponseCode.validateResponse(expectedResponseCode, response.status);
 
     },
 
     async delete(requestURL, expectedResponseCode) {
-        const response = await axios.delete(testResponseCode.baseUrl + requestURL);
+        const response = await axios.delete(baseUrl + requestURL);
         testResponseCode.validateResponse(expectedResponseCode, response.status);
 
     },
